46ce8f2fd56af10a1d6375d55feef8f054215cd2
Rama Dwipa <ramadwipa168@gmail.com>
2026-07-18 23:40:45 +0800

    feat: implement responsive Navbar and initial Hero section with motion animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import "./style/index.css"
+// import { useEffect } from "react";
+// import Lenis from "lenis"
+// import Navbar from "./component/;
+import Hero from "./sections/Hero";
+import Navbar from "./components/ui/Navbar";
+import "./styles/index.css";
 
-function App(){
-    return (
-        <>
-            <h1>Hello World</h1>
-        </>
-    )
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/sections/Hero.tsx
@@ -0,0 +1,11 @@
+import { motion } from "motion/react";
+
+const Hero = () => {
+  return (
+    <section className="min-h-screen pt-24">
+      <h1 className="text-5xl">Hello World</h1>
+    </section>
+  );
+};
+
+export default Hero;
